eff83f778e5b9cb65d813284ea7843a89460f9ed
SN-9527 <github@nireus.cc>
2025-05-19 13:53:28 +0800

    Update ACME证书配置.sh

--- ACME证书配置.sh
+++ ACME证书配置.sh
@@ -6,9 +6,9 @@ reboot
 
 acme.sh --upgrade --auto-upgrade
 
-export CF_Key=""
-export CF_Email=""
+export CF_Key="93741d9df251e8692bcb88ac16b7a21993f45"
+export CF_Email="@outlook.com"
 
-acme.sh --issue --dns dns_cf -d kunlun.asia -d '*.kunlun.asia' --keylength ec-256
+acme.sh --issue --dns dns_cf -d nireus.cc -d '*.nireus.cc' --keylength ec-256
 
-acme.sh --install-cert -d kunlun.asia --key-file /ssl/key1.pem  --fullchain-file /ssl/cert1.pem --reloadcmd "systemctl restart sing-box"
+acme.sh --install-cert -d nireus.cc --key-file /ssl/kkk.pem  --fullchain-file /ssl/ccc.pem --reloadcmd "systemctl restart sing-box"
